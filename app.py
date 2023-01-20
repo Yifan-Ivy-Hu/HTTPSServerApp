80477fc1e258a792bcfaf74fc7f61552c4a9efab
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2023-01-20 20:45:44 +0000

    Add inputs fin html or adding key-vallue pair

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        key = request.form["key"]
+        value = request.form["value"]
+        print("!!!", request.form)
     return render_template('homepage.html')
