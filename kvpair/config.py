022456f687728f84252814ca7b0089bf76c52d9e
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2023-01-20 21:52:21 +0000

    Add config for flask

---
+++ kvpair/config.py
@@ -0,0 +1,19 @@
+"""kvpair development configuration. source of this file: https://eecs485staff.github.io/p2-insta485-serverside/setup_flask.html"""
+
+import pathlib
+
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
+
+# Secret key for encrypting cookies
+SECRET_KEY = b'\xf4\xc82\xd9\xe7+\xf7BA\x92\x8aM>\xc7\x87\xeeQ@\x9d\xd9\t\x824b'
+SESSION_COOKIE_NAME = 'login'
+
+# File Upload to var/uploads/
+KVPAIR_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = KVPAIR_ROOT/'var'/'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+# Database file is var/kvpair.sqlite3
+DATABASE_FILENAME = KVPAIR_ROOT/'var'/'kvpair.sqlite3'
