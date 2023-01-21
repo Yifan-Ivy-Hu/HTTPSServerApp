7b2b101844b2870b7c94c58db903382fb8b95354
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2023-01-21 15:19:05 +0000

    Deal with corner cases

--- kvpair/views/index.py
+++ kvpair/views/index.py
@@ -9,20 +9,11 @@ import flask
 import kvpair
 
 
-@kvpair.app.route('/', methods=['GET', 'POST'])
+@kvpair.app.route('/', methods=['GET'])
 def show_index():
     """Display / route."""
-    # Connect to database
-    connection = kvpair.model.get_db()
 
-    # Query database
-    cur = connection.execute(
-        "SELECT key, value "
-        "FROM kvpairs ",
-    )
-    kvpairs = cur.fetchall()
-    context = {"kvpairs": kvpairs}
-    return flask.render_template("index.html", **context)
+    return flask.render_template("index.html")
 
 @kvpair.app.route('/getall/', methods=['GET', 'POST'])
 def get_all():
@@ -49,11 +40,17 @@ def insert_kvpair():
     context = {}
     if flask.request.method == "POST":
         key = flask.request.form.get('key')
+        # if key already exists, request ignored
+        cur = connection.execute("SELECT value "
+            "FROM kvpairs WHERE key = ?",(key,))
+        existedValue = cur.fetchone()
+        if existedValue != None:
+            return flask.render_template("insert.html", **context)
         value = flask.request.form.get('value')
         connection.execute(
             "INSERT INTO kvpairs(key, value) "
             "VALUES (?, ?)", (key, value,))
-    #TODO: what if key already exists
+
     return flask.render_template("insert.html", **context)
 
 @kvpair.app.route('/delete/', methods=['GET', 'POST'])
@@ -67,7 +64,7 @@ def delete_kvpair():
         key = flask.request.form.get('key')
         connection.execute(
             "DELETE FROM kvpairs WHERE key = ?", (key,))
-    #TODO: what if key does not exist
+    # if key does not exist, request ignored
     return flask.render_template("delete.html", **context)
 
 @kvpair.app.route('/deleteall/', methods=['GET', 'POST'])
