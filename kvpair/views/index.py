f6e268541e406e556025d34cd858e56af61aa2ee
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2023-01-21 02:05:39 +0000

    All post handlers added, corner cases todo

--- kvpair/views/index.py
+++ kvpair/views/index.py
@@ -37,4 +37,47 @@ def insert_kvpair():
         connection.execute(
             "INSERT INTO kvpairs(key, value) "
             "VALUES (?, ?)", (key, value,))
+    #TODO: what if key already exists
     return flask.render_template("insert.html", **context)
+
+@kvpair.app.route('/delete/', methods=['GET', 'POST'])
+def delete_kvpair():
+    # Connect to database
+    connection = kvpair.model.get_db()
+
+    # Query database
+    context = {}
+    if flask.request.method == "POST":
+        key = flask.request.form.get('key')
+        connection.execute(
+            "DELETE FROM kvpairs WHERE key = ?", (key,))
+    #TODO: what if key does not exist
+    return flask.render_template("delete.html", **context)
+
+@kvpair.app.route('/deleteall/', methods=['GET', 'POST'])
+def delete_all_kvpairs():
+    # Connect to database
+    connection = kvpair.model.get_db()
+
+    # Query database
+    context = {}
+    if flask.request.method == "POST":
+        key = flask.request.form.get('key')
+        connection.execute("DELETE FROM kvpairs")
+    return flask.render_template("deleteall.html", **context)
+
+@kvpair.app.route('/getvalue/', methods=['GET', 'POST'])
+def get_value():
+    # Connect to database
+    connection = kvpair.model.get_db()
+
+    # Query database
+    context = {"value": ""}
+    if flask.request.method == "POST":
+        key = flask.request.form.get('key')
+        cur = connection.execute("SELECT value "
+            "FROM kvpairs WHERE key = ?",(key,))
+        value = cur.fetchone()
+        print("!!!", value)
+        context = {"value": value}
+    return flask.render_template("getvalue.html", **context)
