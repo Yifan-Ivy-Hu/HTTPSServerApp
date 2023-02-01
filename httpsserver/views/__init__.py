847d1c85237963256fdbd51a5573cf8bf3f08ed0
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2023-02-01 15:57:18 +0000

    Update comment

--- httpsserver/views/__init__.py
+++ httpsserver/views/__init__.py
@@ -1,5 +1,5 @@
 
-"""Views, one for each kypair page. source: https://eecs485staff.github.io/p2-insta485-serverside/setup_flask.html"""
+"""Views, one for each page. source: https://eecs485staff.github.io/p2-insta485-serverside/setup_flask.html"""
 from httpsserver.views.index import show_index
 from httpsserver.views.fileupload import get_list, upload_file, download_file
 from httpsserver.views.keyvaluepair import get_all, insert_kvpair, delete_kvpair, delete_all_kvpairs, get_value
